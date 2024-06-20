dc5318b2eb2ca4787b1cbc8b0e250d57448aee8a
larissa oliveira <112405867+larissaoliveira22@users.noreply.github.com>
2024-06-20 16:54:04 -0300

    20.06

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal= document.querySelector("caixa-principal");
+const caixaPerguntas= document.querySelector("caixa-perguntas");
+const caixaAlternativa= document.querySelector("caixa-alternativa");
+const caixaResultado= document.querySelector("caixa-resultado");
+const textoResultado= document.querySelector("texto-resultado");
